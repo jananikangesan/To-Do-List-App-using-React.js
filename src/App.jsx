5ba9703b442a69924980452f71b35565d089eaf3
jananikangesan <janakanges1996@gmail.com>
2024-09-26 16:07:31 +0530

    ToDo List

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <div className='max-w-[85vw] h-screen px-4 py-10 mx-auto flex'>
+        <Todo/>
+      </div>
+    </>
+  )
+}
+
+export default App
